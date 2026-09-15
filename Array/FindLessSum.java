cd68679c60e19a4eba24a71d6aab0008aabc69c0
Pratik Dhananjay Dhayagude <pratikdhaygude21@gmail.com>
2026-09-15 19:50:53 +0530

    Update FindLessSum.java

--- Array/FindLessSum.java
+++ Array/FindLessSum.java
@@ -15,6 +15,10 @@
             9
             Array After sorting: [1, 2, 3, 4, 5, 6, 7, 8, 9]
             Total:13
+    -> Time Complexity:
+        O(N)
+    -> Space Complexity:    
+        O(1)
 
 */
 
